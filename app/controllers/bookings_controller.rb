f68aeae2f44ef2e535ca033918a75d078135c0e0
Julie Miguel <pascalinem660@gmail.com>
2021-11-16 11:17:00 +0400

    generated bookings controller and added methods

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,21 @@
+class BookingsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+end
