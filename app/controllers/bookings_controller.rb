b30a60e38ec4a02448abdc5f58c0bd46bcc4e393
Julie Miguel <pascalinem660@gmail.com>
2021-11-16 11:22:58 +0400

    added index and show methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,19 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
   end
 
   def create
   end
 
-  def index
-  end
 
-  def show
-
-  end
 
   def edit
   end
