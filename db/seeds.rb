5ee6a1468a6de560b35ee81a5592084ed95a8823
vilorshini ramsamy <vilorshiniramsamy02@gmail.com>
2021-11-17 17:11:12 +0400

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -143,98 +143,97 @@ Activity.create(
   price: 150
 )
 
-
 Activity.create(
   category_id: sea.id,
-  Name: "Parasailing",
-  Address: "Grand Baie, Trou d’eau Douce, and Ile aux Cerfs",
-  Description: "Parasailing is one of the best water sports in Mauritius. t not just gives you a bird’s-eye view of the lagoons, but also an unmatched thrill. This is the best place to lose yourself in the magnificent turquoise waters and their vastness.",
+  name: "Parasailing",
+  address: "Grand Baie, Trou d’eau Douce, and Ile aux Cerfs",
+  description: "Parasailing is one of the best water sports in Mauritius. t not just gives you a bird’s-eye view of the lagoons, but also an unmatched thrill. This is the best place to lose yourself in the magnificent turquoise waters and their vastness.",
   price: 250
 )
 
 Activity.create(
   category_id: sea.id,
-  Name: "Cave Sea Kayaking",
-  Address: "The Cliffs of Albion in the Mauritius West Coast; starting point is Pointe Aux Sable.",
-  Description:"kayaking allows you to explore mysterious islands and caves. You can opt for a single kayak or a double kayak. The best part is that you can discover a lot more on your own.If starting from Pointe Aux Sable, paddle through the water and discover the gorgeous basalt rock gardens while entering in the stunning rock caves. You will be left spellbound with the beautiful sights you witness on this journey.",
+  name: "Cave Sea Kayaking",
+  address: "The Cliffs of Albion in the Mauritius West Coast; starting point is Pointe Aux Sable.",
+  description:"kayaking allows you to explore mysterious islands and caves. You can opt for a single kayak or a double kayak. The best part is that you can discover a lot more on your own.If starting from Pointe Aux Sable, paddle through the water and discover the gorgeous basalt rock gardens while entering in the stunning rock caves. You will be left spellbound with the beautiful sights you witness on this journey.",
   price: 1000
 )
 
 Activity.create(
   category_id: sea.id,
-  Name: "Water Skiing",
-  Address: "Trou d’eau Douce",
-  Description: "Water skiing in Mauritius is a wonderful sport to get an adrenaline rush. This high-octane sport seems a tad difficult at first, but soon it becomes a smooth sail. All you need to do is skim along the water’s surface, standing upright on skis holding a tow rope. Water-ski is best enjoyed where the water is calm. This water sport will need you to use a lot of energy, so keep your energy drinks handy.",
+  name: "Water Skiing",
+  address: "Trou d’eau Douce",
+  description: "Water skiing in Mauritius is a wonderful sport to get an adrenaline rush. This high-octane sport seems a tad difficult at first, but soon it becomes a smooth sail. All you need to do is skim along the water’s surface, standing upright on skis holding a tow rope. Water-ski is best enjoyed where the water is calm. This water sport will need you to use a lot of energy, so keep your energy drinks handy.",
   price: 1500
 )
 
 Activity.create(
   category_id: sea.id,
-  Name: "Windsurfing",
-  Address: "Le Morne peninsula",
-  Description: "Windsurfing in Mauritius is exciting as well as challenging. Besides being fun, it’s a great form of workout and can burn upto 1000 calories per hour. That’s good for a sport, right?",
+  name: "Windsurfing",
+  address: "Le Morne peninsula",
+  description: "Windsurfing in Mauritius is exciting as well as challenging. Besides being fun, it’s a great form of workout and can burn upto 1000 calories per hour. That’s good for a sport, right?",
   price:1000
 )
 Activity.create(
   category_id: sea.id,
-  Name: "Blue Safari Submarine",
-  Address:  "Trou Aux Biches",
-  Description: "Wish to dive, but don’t want to get wet? Try Blue Safari Submarine ride. It is a different one among Mauritius water sports, and takes you among the exotic corals and the ship-wreck ‘Star Hope’. Blue Safari Submarine will give you a whirlwind tour of the ocean. So, this like a aquarium but with a twist, as you are inside it!",
-  price: 2,000
-)
-Activity.create(
-  category_id: sea.id,
-  Name: "Dolphin Spotting And Whale Watching",
-  Address: "The coast of Tamarin",
-  Description: "Dolphin spotting and whale watching in Mauritius definitely makes your trip unforgettable. It requires a bit of patience and hunting, but the result is priceless. Its a great feeling to witness the giants of the ocean, in their natural environment.",
-  price: 2,500
-)
-Activity.create(
-  category_id: sea.id,
-  Name: "Underwater Sea Walk",
-  Address: "Trou d’eau Douce",
-  Description: "Try underwater sea walking if you want to explore the rich sea life. It might be little scary at the beginning, but it’s worth it. Compared to other water sports activities in Mauritius, this is gives you the best underwater experience. Going out on a walk on the sea floor can also be a romantic thing to do for sporty couples.",
+  name: "Blue Safari Submarine",
+  address:  "Trou Aux Biches",
+  description: "Wish to dive, but don’t want to get wet? Try Blue Safari Submarine ride. It is a different one among Mauritius water sports, and takes you among the exotic corals and the ship-wreck ‘Star Hope’. Blue Safari Submarine will give you a whirlwind tour of the ocean. So, this like a aquarium but with a twist, as you are inside it!",
   price: 2000
 )
 Activity.create(
   category_id: sea.id,
-  Name: "Scuba Diving",
-  Address: "Roche Zozo located off Blue Bay on the southern coast of Mauritius.",
-  Description: "Enjoy the colorful marine life and the peaceful silence of the sea as you dive into it.",
+  name: "Dolphin Spotting And Whale Watching",
+  address: "The coast of Tamarin",
+  description: "Dolphin spotting and whale watching in Mauritius definitely makes your trip unforgettable. It requires a bit of patience and hunting, but the result is priceless. Its a great feeling to witness the giants of the ocean, in their natural environment.",
+  price: 2500
+)
+
+Activity.create(
+  category_id: sea.id,
+  name: "Underwater Sea Walk",
+  address: "Trou d’eau Douce",
+  description: "Try underwater sea walking if you want to explore the rich sea life. It might be little scary at the beginning, but it’s worth it. Compared to other water sports activities in Mauritius, this is gives you the best underwater experience. Going out on a walk on the sea floor can also be a romantic thing to do for sporty couples.",
   price: 2000
 )
 
+Activity.create(
+  category_id: sea.id,
+  name: "Scuba Diving",
+  address: "Roche Zozo located off Blue Bay on the southern coast of Mauritius.",
+  description: "Enjoy the colorful marine life and the peaceful silence of the sea as you dive into it.",
+  price: 2000
+)
 
 Activity.create(
   category_id: eco.id,
-  Name: "Ile aux Aigrettes Nature Reserve Tour",
-  Address: "Blue Bay Beach",
-  Description:"This is a full-day tour in a private air-conditioned vehicle with a friendly driver, with the highlight being the Ile aux Aigrettes Nature Reserve. You will be treated to a guided 1.5 to 2 hour easy hike around the island where you will be able to discover the unique flora and fauna endemic to Mauritius. You will also have the chance to visit the white sandy beach, and have a dip in the beautiful lagoon, of Blue Bay and wander around the historical village of Mahébourg.",
-  price: 4,891
+  name: "Ile aux Aigrettes Nature Reserve Tour",
+  address: "Blue Bay Beach",
+  description:"This is a full-day tour in a private air-conditioned vehicle with a friendly driver, with the highlight being the Ile aux Aigrettes Nature Reserve. You will be treated to a guided 1.5 to 2 hour easy hike around the island where you will be able to discover the unique flora and fauna endemic to Mauritius. You will also have the chance to visit the white sandy beach, and have a dip in the beautiful lagoon, of Blue Bay and wander around the historical village of Mahébourg.",
+  price: 4891
 )
+
 Activity.create(
   category_id: eco.id,
-  Name: "Exotic South: A Journey into the Nature",
-  Address: "Grand Baie",
-  Description: "In this tour, you will explore the exotic south-west of Mauritius on this 8 hours Nature tour.With your certified guide, you will be able to visit some of the highlights and beautiful places of the South-West of the paradise island.",
-  price: 5,474
+  name: "Exotic South: A Journey into the Nature",
+  address: "Grand Baie",
+  description: "In this tour, you will explore the exotic south-west of Mauritius on this 8 hours Nature tour.With your certified guide, you will be able to visit some of the highlights and beautiful places of the South-West of the paradise island.",
+  price: 5474
 )
+
 Activity.create(
   category_id: eco.id,
-  Name: "DISCOVER PERMACULTURE",
-  Address: "La Croisette, MU, Chem. Vingt Pieds, Grand Baie 30517",
-  Description: "Discover Mauritius off the beaten track, discover sustainable development initiatives in exceptional environments, meet and share with the Mauritian population! Discover permaculture, an ethical system for developing sustainable and autonomous agriculture, and let's share in the heart of the garden, in the middle of the sugar cane fields, a meal made exclusively with garden products.",
+  name: "DISCOVER PERMACULTURE",
+  address: "La Croisette, MU, Chem. Vingt Pieds, Grand Baie 30517",
+  description: "Discover Mauritius off the beaten track, discover sustainable development initiatives in exceptional environments, meet and share with the Mauritian population! Discover permaculture, an ethical system for developing sustainable and autonomous agriculture, and let's share in the heart of the garden, in the middle of the sugar cane fields, a meal made exclusively with garden products.",
   price: 500
 )
 
-
-
 Activity.create(
-  category_id: food.id,
+  category_id: eco.id,
   name: "Rum Tasting at Rhumerie de Chamarel",
   address: "Chamarel, South West",
-  description: "Enjoy a tour and a unique rum tasting session where you can try a selection of agricultural rums produced by the distillery. Displaying a special sense of warmth and authenticity, the Rhumerie de 
-  Chamarel welcomes visitors for an all-encompassing tourism experience ranging from a guided visit of the distillery and rum tasting to a tasty meal at L'Alchimiste, its own stylish and creative restaurant.",
+  description: "Enjoy a tour and a unique rum tasting session where you can try a selection of agricultural rums produced by the distillery. Displaying a special sense of warmth and authenticity, the Rhumerie de Chamarel welcomes visitors for an all-encompassing tourism experience ranging from a guided visit of the distillery and rum tasting to a tasty meal at L'Alchimiste, its own stylish and creative restaurant.",
   price: 1200
 ​)
 ​
@@ -242,59 +241,47 @@ Activity.create(
   category_id: food.id,
   name: "Wine Tasting at Takamaka Boutique Winery",
   address: "Vacoas, Centre",
-  description: "Your guided tour of the Takamaka Winery, the only winery in the world dedicated exclusively to lychee wine. A wine tasting session will allow you to 
-  sample a fine selection of exotic flavours and try delicious wines while learning about wine production from the owner himself",
+  description: "Your guided tour of the Takamaka Winery, the only winery in the world dedicated exclusively to lychee wine. A wine tasting session will allow you to sample a fine selection of exotic flavours and try delicious wines while learning about wine production from the owner himself",
   price: 1000
 )  ​
-  ​
+
 Activity.create(
   category_id: food.id,
   name: "Wine Tasting at La Joliette",
   address: "Petite Riviere, West",
-  description: "La Joliette Winery welcomes you for a visit where you can taste new exciting and innovative experiences in the wine industry including the Tropical Island Fruit Wines.
-  Whether you are just having your first sips of wine or you are a seasoned sommelier, you will get a complete wine experience during your visit at La Joliette.",
+  description: "La Joliette Winery welcomes you for a visit where you can taste new exciting and innovative experiences in the wine industry including the Tropical Island Fruit Wines. Whether you are just having your first sips of wine or you are a seasoned sommelier, you will get a complete wine experience during your visit at La Joliette.",
   price: 900
 )  ​
-  ​
+
 Activity.create(
   category_id: food.id,
   name: "Port Louis Street Food Tour",
   address: "Port Louis",
-  description: "Your stay in Mauritius will not be complete if you miss out on street food, ranking in the world Top 10, according to The Telegraph!
-  There are also original drinks that will refresh you and perfectly pair some of the food you will taste. There is the famous Mauritian tea, hot black tea with sugar and milk, 
-  the delicious Alouda, sweetened flavoured milk with tukmaria seeds and grated agar-agar jelly, the best are found in markets, and the various tamarind or lemongrass juices, and 
-  of course, the local beer and the limitless rum-based drinks evoking flavours of holidays!",
+  description: "Your stay in Mauritius will not be complete if you miss out on street food, ranking in the world Top 10, according to The Telegraph! There are also original drinks that will refresh you and perfectly pair some of the food you will taste. There is the famous Mauritian tea, hot black tea with sugar and milk, the delicious Alouda, sweetened flavoured milk with tukmaria seeds and grated agar-agar jelly, the best are found in markets, and the various tamarind or lemongrass juices, and of course, the local beer and the limitless rum-based drinks evoking flavours of holidays!",
   price: 1500
 )
-  ​
+
 Activity.create(
   category_id: food.id,
   name: "Full-Day Culinary Experience with Local Chef",
   address: "Grand Bay, North",
-  description: "With the help of your native host, Chef Sandy, you will discover the BEST of Mauritian unique flavors and local cuisines while learning traditional ways of life. 
-  For a whole day, you will enjoy an authentic Mauritian experience.",
+  description: "With the help of your native host, Chef Sandy, you will discover the BEST of Mauritian unique flavors and local cuisines while learning traditional ways of life. For a whole day, you will enjoy an authentic Mauritian experience.",
   price: 2000
 )  ​
-  ​
-  ​
-  ​
-  ​
+
 Activity.create(
   category_id: team.id,
   name: "Room Escape Game at Dodoquest",
   address: "Moka, Centre",
-  description: "The Dodoquest offers you a real situation of adventures, amusements, entertainment, and mind boggling puzzles to solve. Your family, co-workers
-  or friends and you will enjoy an incredible adventure in Mauritius. The escape quest is inspired by unique scenarios of Mauritian colonial history and the experience offers you
-  an extraordinary opportunity of being transfered into the past.",
+  description: "The Dodoquest offers you a real situation of adventures, amusements, entertainment, and mind boggling puzzles to solve. Your family, co-workers or friends and you will enjoy an incredible adventure in Mauritius. The escape quest is inspired by unique scenarios of Mauritian colonial history and the experience offers you an extraordinary opportunity of being transfered into the past.",
   price: 1200
 )  ​
- 
+
 ​Activity.create(
-    category_id: team.id,
+  category_id: team.id,
   name: "Laser Tag Game",
   address: "Quatre Bornes, Centre",
-  description: "The laser tag game can be played indoor, outdoor or at a public loaction. It is an ideal and exciting activity for team building with friends, family  
-  and co-workers and requires team strategy.",
+  description: "The laser tag game can be played indoor, outdoor or at a public loaction. It is an ideal and exciting activity for team building with friends, family and co-workers and requires team strategy.",
   price: 900
 )
 ​
@@ -302,17 +289,15 @@ Activity.create(
   category_id: team.id,  ​
   name: "Treasure Hunt",
   address: "Around the island",
-  description: "Each team consists of 4-5 people who must use brains and muscles to find the final treasure. A car will be provided to each team and the search may
-  lead the teams to anywhere around the island. The only additional tool will be a smartphone.",
+  description: "Each team consists of 4-5 people who must use brains and muscles to find the final treasure. A car will be provided to each team and the search may lead the teams to anywhere around the island. The only additional tool will be a smartphone.",
   price: 1500
 )​
- 
+
 Activity.create(
   category_id: team.id,​
   name: "Pirates of the East Hunt",
   address: "East Coast",
-  description: "Pirates of the East is a fun and challenging modern pirates treasure hunt set in the lagoons of the east coast of Mauritius. Embark on a catamaran and become
-  a ruthless and witty pirate for a day, if you survive the challenges.",
+  description: "Pirates of the East is a fun and challenging modern pirates treasure hunt set in the lagoons of the east coast of Mauritius. Embark on a catamaran and become a ruthless and witty pirate for a day, if you survive the challenges.",
   price: 1100
 )
 
