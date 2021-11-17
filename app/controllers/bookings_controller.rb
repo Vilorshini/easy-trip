6d1b0c884f256e42d6f904e88abfcf91d3b0cf06
Christian BeauxYeux <gildweek@gmail.com>
2021-11-17 15:29:39 +0400

    Added new redirected path in bookings create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,7 +20,7 @@ class BookingsController < ApplicationController
     @booking.activity = @activity.first
     @booking.user = current_user
     if @booking.save
-      redirect_to root_path
+      redirect_to bookings_path
     else
       render :new
     end
