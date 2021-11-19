bb60c760a7732e6837214f3c73dd890886dd6f39
Bhavna Basenoo <bhavnabasenoo@yahoo.com>
2021-11-19 12:02:30 +0400

    added an about page along with html and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
 
-  resources :activities 
+  resources :activities
   resources :reviews, only: :create
-  resources :categories, only: :show do 
+  resources :categories, only: :show do
     resources :bookings, only: [:new, :show, :create], except: :index
   end
   resources :bookings, only: :index
