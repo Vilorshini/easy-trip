f9a6a6e8a9ce5adad0fdd0150dee743cdaf87f7a
Christian BeauxYeux <gildweek@gmail.com>
2021-11-23 13:13:03 +0400

    Added photo to bottom-nav bar

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -3,13 +3,13 @@ const initNavbar = () => {
     let list = document.querySelectorAll(".list");
     
     for (let i = 0; i < list.length; i++) {
-    list[i].onclick = (e) => {
-        let j = 0;
-        while (j < list.length) {
-        list[j++].className = "list";
-        }
-        list[i].className = "list active";
-    };
+        list[i].onclick = (e) => {
+            let j = 0;
+            while (j < list.length) {
+            list[j++].className = "list";
+            }
+            list[i].className = "list active";
+        };
     }
 
     list.forEach((elements) => {
